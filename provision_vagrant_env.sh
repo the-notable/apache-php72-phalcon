73b255683a5125e14bad9c60710dd52e0c254779
the-notable <danielwkennedy@outlook.com>
2018-11-06 22:05:57 -0800

    push

--- provision_vagrant_env.sh
+++ provision_vagrant_env.sh
@@ -17,6 +17,6 @@ add-apt-repository \
    stable"
 apt-get update && apt-get install -y docker-ce
 
-# Install docker-compose
+#  Install docker-compose
 #curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #chmod +x /usr/local/bin/docker-compose
